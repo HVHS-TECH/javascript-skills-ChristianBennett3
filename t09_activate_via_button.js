8bbc24fa24443153f72661cc0308e59ec69863f0
ChristianBennett3 <24204cb@hvhs.school.nz>
2026-05-05 09:56:05 +1200

    T10 Done

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -1,5 +1,5 @@
 /****************************
-Name of Task: Functions with parametersx - Task 9
+Name of Task: Activate Via Buttons - Task 9
 ****************************/
 
 console.log("Running task 09")
@@ -10,7 +10,7 @@ var age = 15
 var pocketMoney = 50
 var year = 2026
 var yearBorn = year - age
-const output = document.getElementById("JavaScriptOutput")
+const output = document.getElementById("spaceForJavaScriptOutput")
 
 
 /****************************
@@ -19,20 +19,7 @@ Main code
 
 
 
-output.innerHTML = "<h2>Hi " + usersName + "</h2>"
-output.innerHTML += "<p>As of " + year + " you are " + age + " years old.</p>"
-output.innerHTML += "<p>You have " + pocketMoney + " dollars</p>"
-output.innerHTML += "<p>You were born in " + yearBorn + "</p>"
-age = age + 10
-output.innerHTML += "<p>In ten years you will be " + age + "</p>"
-pocketMoney = pocketMoney / 2 
-output.innerHTML += "<p>You spend half your money, now you have $" + pocketMoney + "</p>"
-pocketMoney = pocketMoney + 3
-output.innerHTML += "<p>Then you get $3, now you have $" + pocketMoney + "</p>"
-welcome()
-displayProduct("Chocolate bar", 4)
-displayProduct("Chips", 3)
-displayProduct("Drink", 2.50)
+
 
 
 
@@ -49,5 +36,22 @@ function welcome(){
 
 function displayProduct(_name, _price){
     output.innerHTML += "<p>" + _name + ":" + " $" + _price + "</p>"
+}
+
+function start(){
+    output.innerHTML = "<h2>Hi " + usersName + "</h2>"
+    output.innerHTML += "<p>As of " + year + " you are " + age + " years old.</p>"
+    output.innerHTML += "<p>You have " + pocketMoney + " dollars</p>"
+    output.innerHTML += "<p>You were born in " + yearBorn + "</p>"
+    age = age + 10
+    output.innerHTML += "<p>In ten years you will be " + age + "</p>"
+    pocketMoney = pocketMoney / 2 
+    output.innerHTML += "<p>You spend half your money, now you have $" + pocketMoney + "</p>"
+    pocketMoney = pocketMoney + 3
+    output.innerHTML += "<p>Then you get $3, now you have $" + pocketMoney + "</p>"
+    welcome()
+    displayProduct("Chocolate bar", 4)
+    displayProduct("Chips", 3)
+    displayProduct("Drink", 2.50)
 }
 
